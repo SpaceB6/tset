bc391d0a7d596325c5c7ee318deca84a7fd4d3ee
SpaceB6 <spaceb6vr@gmail.com>
2025-04-11 14:41:45 +0930

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -7,3 +7,7 @@ const firebaseConfig = {
   appId: "1:660768350715:web:c63713a51ade874d0deb5e",
   measurementId: "G-43LGFTBXRQ"
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
