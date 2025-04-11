1378d71f501e54017825971dcca989d0e7f4097f
SpaceB6 <spaceb6vr@gmail.com>
2025-04-11 14:41:32 +0930

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA4yRkCYzUDc_GY_ou1UYJ7bIytLa-DwJY",
+  authDomain: "dynablocks-ce7d8.firebaseapp.com",
+  projectId: "dynablocks-ce7d8",
+  storageBucket: "dynablocks-ce7d8.firebasestorage.app",
+  messagingSenderId: "660768350715",
+  appId: "1:660768350715:web:c63713a51ade874d0deb5e",
+  measurementId: "G-43LGFTBXRQ"
+};
